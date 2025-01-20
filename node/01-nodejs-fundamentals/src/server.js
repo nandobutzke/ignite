e518953d3c6aeffe22f313d1039d4cacf6883b2e
Fernando Butzke <fernandobutzke1@gmail.com>
2025-01-20 10:08:23 -0300

    feat: Adding database persistence

--- node/01-nodejs-fundamentals/src/server.js
+++ node/01-nodejs-fundamentals/src/server.js
@@ -1,38 +1,34 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
+import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = [];
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    req.body = null
-  }
+  await json(req, res);
 
   if (method === 'GET' && url === '/users') {
+    const users = database.select('users')
+
     return res
-          .setHeader('Content-type', 'application/json')
           .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
-      id: 1,
+    const user = {
+      id: randomUUID(),
       name,
       email
-    })
+    }
 
-    return res.writeHead(201).end('Criação do usuário')
+    database.insert('users', user)
+
+    return res.writeHead(201).end()
   }
 })
 
